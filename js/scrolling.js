a7101c4b37b50ad04140afe4d947dc664e367afd
Robert Ozimek <robertozimek90@gmail.com>
2019-03-17 10:02:07 -0400

    Updated personal site, switched to using npm over brower, and updated bootstrap version

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-    smoothScroll.init();
+window.scroll({
+    top: 2500,
+    left: 0,
+    behavior: 'smooth'
+});
+
+window.scrollBy({
+    top: 100,
+    left: 0,
+    behavior: 'smooth'
 });
